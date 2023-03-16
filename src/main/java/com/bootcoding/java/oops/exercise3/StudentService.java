3dd434d5ba5ba0f45c2820419f4115681bf20744
Rina Pokale <rinapokale009@gmail.com>
2023-03-16 20:49:17 +0530

    fetch those students who match particular criteria of percentage

--- src/main/java/com/bootcoding/java/oops/exercise3/StudentService.java
+++ src/main/java/com/bootcoding/java/oops/exercise3/StudentService.java
@@ -4,6 +4,7 @@ public class StudentService {
     public Student[] findStudentAboveCriteriaOfPer(Student[] students, float criteriaOfPer){
         int count = 0;
         Student[] s = new Student[10];
+
         for(int i = 0; i < students.length; i++){
             if(students[i].percentage > criteriaOfPer)
                 s[count++] = students[i];
