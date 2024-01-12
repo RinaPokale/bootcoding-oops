04d847c477c35eaabb0e978346d63536ec16153b
Rina Pokale <rinapokale009@gmail.com>
2024-01-12 22:46:00 +0530

    Dynamic Method Dispatch

---
+++ src/main/java/com/bootcoding/java/oops/polymorphism/runtime/Bike.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.oops.polymorphism.runtime;
+
+public class Bike {
+    void run(){
+        System.out.println("bike running safely");
+    }
+
+    void start(){
+        System.out.println("started");
+    }
+}
