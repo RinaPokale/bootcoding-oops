34e170386e04bf6e9950ec2b3863d9789a9da280
Rina Pokale <rinapokale009@gmail.com>
2024-01-13 22:26:24 +0530

    abstraction

---
+++ src/main/java/com/bootcoding/java/oops/abstraction/exercise1/Bicycle.java
@@ -0,0 +1,8 @@
+package com.bootcoding.java.oops.abstraction.exercise1;
+
+public class Bicycle extends Vehicle {
+    @Override
+    void speed() {
+        System.out.println("running at a high speed!!");
+    }
+}
