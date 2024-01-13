ec8c25302401b06de9ba16ea32e6ca5e4c6ee811
Rina Pokale <rinapokale009@gmail.com>
2024-01-13 22:19:27 +0530

    abstraction

---
+++ src/main/java/com/bootcoding/java/oops/abstraction/exercise1/Test.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.oops.abstraction.exercise1;
+
+public class Test {
+    public static void main(String[] args) {
+        Bicycle b = new Bicycle();
+        b.start();
+        b.speed();
+
+        Vehicle v = new Bicycle();
+        v.start();
+        v.speed();
+    }
+}
