58ead77b223f98a26bb1a25ecdfda1e3e7bfd3bc
Rina Pokale <rinapokale009@gmail.com>
2024-01-13 22:24:44 +0530

    abstraction

---
+++ src/main/java/com/bootcoding/java/oops/abstraction/exercise1/Vehicle.java
@@ -0,0 +1,7 @@
+package com.bootcoding.java.oops.abstraction.exercise1;
+public abstract class Vehicle {
+    abstract void speed();
+    void start(){
+        System.out.println("Vehicle started!!!!");
+    }
+}
