0012edf08f326e7edc2bb572f1385d06b0525378
Rina Pokale <rinapokale009@gmail.com>
2024-01-14 22:34:19 +0530

    coupling

---
+++ src/main/java/com/bootcoding/java/oops/coupling/Author.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java.oops.coupling;
+
+//Coupling:- one class is dependent on another class
+public class Author {
+    String name;
+    String education;
+    long phone;
+
+
+}
+
+
---
+++ src/main/java/com/bootcoding/java/oops/coupling/Book.java
@@ -0,0 +1,8 @@
+package com.bootcoding.java.oops.coupling;
+
+public class Book {
+    String tittle;
+    String publisher;
+    double price;
+    Author author; // has a relation
+}
